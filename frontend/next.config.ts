63ffef0e559955bcc786b81e5c9eb743756dedb1
anasmemon226 <anusm226@gmail.com>
2025-06-12 01:51:10 +0500

    Fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
+  /* config options here */
+  output: "export"
 };
 
 export default nextConfig;
