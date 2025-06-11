5a9425b548119c3d9ed66b713f602d28dd65b46d
anasmemon226 <anusm226@gmail.com>
2025-06-11 16:24:51 +0500

    Key name changed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region"      { default = "us-east-1" }
 variable "instance_type"   { default = "t2.micro" }
-variable "key_name"        { default = "my-key" }
-variable "public_key_path" { default = "my-key.pub"}
+variable "key_name"        { default = "swarm-key" }
+variable "public_key_path" { default = "swarm_key.pub"}
 variable "ami_id"          { default = "ami-0731becbf832f281e" }
